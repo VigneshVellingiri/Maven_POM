ba56f71599a39d7a06a91196fd01809c6913549b
vigne <vigne@VIGNESH-DESKTOP-M7BTJCA>
2023-10-14 04:00:38 +0530

    Test Class file created

---
+++ formfilling/src/test/java/test/myContact.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+
+public class myContact {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
