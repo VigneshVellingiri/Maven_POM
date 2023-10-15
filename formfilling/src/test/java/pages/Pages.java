677d5add9fec5283d391a0f9e0cb33e9f8b2434a
vigne <vigne@VIGNESH-DESKTOP-M7BTJCA>
2023-10-15 13:13:53 +0530

    TC_002 added

--- formfilling/src/test/java/pages/Pages.java
+++ formfilling/src/test/java/pages/Pages.java
@@ -1,5 +1,23 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Pages {
+	public static WebDriver driver;
+	
+	@FindBy(xpath = "(//input[@name='user'])[1]")
+	public WebElement UserName;
+	
+	@FindBy(xpath = "//input[@name='pass']")
+	public WebElement Password;
+	
+	
+	public Pages( WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
 
 }
--- formfilling/src/test/java/test/myContact.java
+++ formfilling/src/test/java/test/myContact.java
@@ -2,13 +2,16 @@ package test;
 
 import org.testng.annotations.Test;
 
+import pages.Pages;
 import utilities.TestSetup;
 
 import java.util.Properties;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
@@ -18,14 +21,37 @@ public class myContact {
 	public static Properties prop;
 	TestSetup obj = new TestSetup();
 	
-  @Test
+  
+  @Test(groups = {"demo"})
   public void USER_LOGIN() {
+	  Reporter.log("Correct User name and password");
+	  Pages page = new Pages(driver);
 	  
 	  String URL = prop.getProperty("url");
 	  
 	  driver.get(URL);
-	  driver.findElement(By.xpath("(//input[@name='user'])[1]")).sendKeys("Vignesh");
+	  page.UserName.sendKeys("Vignesh@541");
+	  page.Password.sendKeys("v@321");
+	  
   }
+  @Test(groups = {"demo"})
+  public void TC_002() {
+	  Reporter.log("Wrong User name and password");
+	  Pages page = new Pages(driver);
+	  String URL = prop.getProperty("url");
+	  
+	  driver.get(URL);
+	  page.UserName.sendKeys("Vignesh@69");
+	  page.Password.sendKeys("v@32");
+	  
+	  
+  }
+  
+  @AfterTest
+  public void AfterTest() {
+	  
+  }
+  
   @BeforeClass
   public void beforeClass() {
 	  this.driver = obj.driver;
